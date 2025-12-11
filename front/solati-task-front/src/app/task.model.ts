f4ce58f1e8ec82dc86e2e918a573b01298c6afde
David Mc Ewen <dmcewena@hotmail.com>
2025-12-10 23:46:08 -0500

    limpiando

--- front/solati-task-front/src/app/task.model.ts
+++ front/solati-task-front/src/app/task.model.ts
@@ -1,4 +1,3 @@
-// src/app/task/task.model.ts
 export interface Task {
   id: number;
   title: string;
--- front/solati-task-front/src/app/task.service.ts
+++ front/solati-task-front/src/app/task.service.ts
@@ -1,4 +1,4 @@
-// src/app/task/task.service.ts
+
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
